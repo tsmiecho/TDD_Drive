27dcaabde8a8de03c04d40f4936252cabf8d9594
Viktor Farcic <viktor@farcic.com>
2015-04-11 12:47:21 +0200

    Removed unused imports

--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -2,9 +2,6 @@ package com.packtpublishing.tddjava.ch04ship;
 
 import org.testng.annotations.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.testng.Assert.*;
 
 @Test
